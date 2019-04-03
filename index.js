34a4c9e6ffc4f1d38f77b6023f28e589a2cff29e
VictorParraCant <victor.parracant@gmail.com>
2019-04-03 15:28:16 +0200

    Inital commit of the new website

---
+++ index.js
@@ -0,0 +1,16 @@
+// import { TweenMax, TimelineMax } from 'gsap'
+//
+// const app = document.querySelector('#app')
+//
+// const timeline = new TimelineMax()
+//
+//   timeline.pause()
+//
+//   timeline.to('#app', 0.5, { x: 100 })
+//   timeline.to('#app', 0.5, { y: 100 })
+//   timeline.to('#app', 0.5, { x: 50 })
+//   timeline.to('#app', 0.5, { y: 50 })
+//
+// app.addEventListener('click', () => {
+//   timeline.resume()
+// })
